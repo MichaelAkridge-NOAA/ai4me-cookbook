91a1e69a8c156801f7a17b3665bfe3167ba99487
MichaelAkridge-NOAA <michael.akridge@noaa.gov>
2025-03-03 16:14:44 -1000

    test

---
+++ .devcontainer/label-studio-demo/post_start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Construct the message
+message="## [Click to Open Label Studio | Tool](https://$CODESPACE_NAME-8080.app.github.dev)"
+echo "Debug: Constructed message."
+
+# Echo the message to the terminal
+echo "$message"
+
+# Check if the message is already in README.md and append if it's not
+echo "Debug: Checking if message is in README.md."
+if ! grep -Fqx "$message" /app/README.md; then
+    echo "Debug: Message not found in README.md, appending now."
+    echo "$message" >> /app/README.md
+else
+    echo "Debug: Message already in README.md, not appending."
+fi
+
+sleep 10
